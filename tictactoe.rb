d2900f58b5ff9b499b31306e5f3b1aa30c5f760d
beersheep <beersheep0109@gmail.com>
2015-11-06 00:11:37 +0800

    added current player and change player method

--- tictactoe.rb
+++ tictactoe.rb
@@ -44,30 +44,47 @@ class Player
 end
 
 class Game
+  attr_accessor :current_player
+
   def initialize 
     @player = Player.new("Roy", "O")
     @computer = Player.new("Computer", "X")
     @board = Board.new
+    @current_player = @player
   end
 
   def pick_a_square
-    puts "Please select a square(1-9):"
-    choice = gets.chomp.to_i
-    begin
-      if !@board.empty_square.include?(choice)
+    if @current_player == @player
+      puts "Please select a square(1-9):"
+      choice = gets.chomp.to_i
+      begin
+        if !@board.empty_square.include?(choice)
         puts "The square has been taken, please select other squares."
         choice = gets.chomp.to_i
-        end until @board.empty_square.include?(choice)
-      end
-    @board[choice] = "O"
+      end until @board.empty_square.include?(choice)
+        end
+      @board[choice] = "O"
+    else
+      choice = @board.empty_square.sample
+      @board[choice] = "X"
+    end
+  end
+
+  def change_player
+    if @current_player == @computer
+      @current_player = @player
+    else @current_player = @computer
+    end
   end
 
   def play
     @board.draw_board
     
     pick_a_square
+    change_player
+    pick_a_square
     @board.draw_board
-      pick_a_square
+      
   end
 
 end
